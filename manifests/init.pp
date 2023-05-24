114c4a37f5d1599b9b90785b71b00a1ab3419b43
Philippe GANZ <pganz@pictet.com>
2023-05-24 16:02:16 +0200

    Rewrite stunnel class in a modern Puppet way

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,76 @@
-# == Class: stunnel
+# 
+# @summary 
+#   Basic Stunnel config. 
+#   Installs the packages and creates essential directories.
+# 
+# @param bin_name
+#   Name of the stunnel executable.
+# 
+# @param bin_path
+#   Path to the directory containing the stunnel executable.
+# 
+# @param cert_dir
+#   Path to the directory containing the certificates.
+# 
+# @param config_dir
+#   Path to the directory containing the configuration files.
+# 
+# @param log_dir
+#   Path to the directory containing the output files.
+# 
+# @param packages
+#   List of packages to install.
+# 
+# @param pid_dir
+#   Path to the directory containing the pid file. 
+#   Linux only.
+# 
+# @param service_init_system
+#   System service responsible to launch the stunnel service.
+#   Linux only.
+# 
+# @param user
+#   User that will own the files and run the service.
+# 
+# @param group
+#   Group that will own the files and run the service.
+# 
+# @example Basic usage
+#   include stunnel
+#   
+# @author 
+#   Aaron Russo
+#   Stephen Hoekstra
+#   Philippe Ganz
 #
-# STunnel Management
+# @api public
 #
-class stunnel {
-  file { '/usr/local/bin/stunnel-combine-certs':
-    ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0555',
-    source => 'puppet:///modules/stunnel/stunnel-combine-certs.rb',
-  }
+# @since 0.0.0
+# 
+class stunnel (
+  Optional[String]               $bin_name            = undef,
+  Optional[Stdlib::Absolutepath] $bin_path            = undef,
+  Optional[Stdlib::Absolutepath] $cert_dir            = undef,
+  Optional[Stdlib::Absolutepath] $config_dir          = undef,
+  Optional[Stdlib::Absolutepath] $log_dir             = undef,
+  Optional[Array]                $packages            = undef,
+  Optional[Stdlib::Absolutepath] $pid_dir             = undef,
+  Optional[String]               $service_init_system = undef,
+  Optional[String]               $user                = undef,
+  Optional[String]               $group               = undef,
+) {
+  ensure_packages( $packages )
 
-  include stunnel::install, stunnel::config
-  Class['Stunnel::Install'] -> Class['Stunnel::Config']
+  $stunnel_dirs = [
+    $cert_dir,
+    $config_dir,
+    $log_dir,
+  ]
+
+  file { $stunnel_dirs:
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
+    mode   => '0775',
+  }
 }
