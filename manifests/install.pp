b49dbf387bd81fba87cc712ce5e4835d1ff97740
Philippe GANZ <pganz@pictet.com>
2023-05-24 15:59:37 +0200

    Drop the install class, move to init instead

--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class: stunnel::install
-#
-# Installs the required packages for stunnel and this module
-#
-class stunnel::install {
-  require stunnel::data
-
-  ensure_packages( $stunnel::data::package )
-}
